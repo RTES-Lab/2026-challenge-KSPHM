ff6cc069aad9b453798edb7db10bf02983590f21
RTES-Lab <uosrtes@gmail.com>
2026-04-14 00:07:43 +0900

    tmp: download 함수(미완)

---
+++ funs/download.py
@@ -0,0 +1,27 @@
+import gdown
+import zipfile
+import os
+import glob
+
+
+def download_dataset(root: str):
+    # TODO: df 만드는거 해야함
+    os.makedirs(root, exist_ok=True)
+
+    # 다운로드
+    url = "https://drive.google.com/uc?id=1ahTu-haFuwS7orW2Dpt8kZG9Di563sNB"
+    output = os.path.join(root, "Train.zip")
+    gdown.download(url, output, quiet=False)
+
+    # 압축 해제
+    with zipfile.ZipFile(output, 'r') as z:
+        z.extractall(root)
+        print(f"압축 해제 완료: {len(z.namelist())}개 파일")
+    os.remove(output)
+
+    # 내부 zip 파일들도 압축 해제
+    for zf in glob.glob(os.path.join(root, "*.zip")):
+        with zipfile.ZipFile(zf, 'r') as z:
+            z.extractall(root)
+        os.remove(zf)
+        print(f"{os.path.basename(zf)} 압축 해제 완료")
