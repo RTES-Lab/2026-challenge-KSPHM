fba772ff4e69cf399e7c9ee8addaba8d1e0a064e
RTES-Lab <uosrtes@gmail.com>
2026-04-16 15:23:14 +0900

    tmp: 0416 백업

--- funs/download.py
+++ funs/download.py
@@ -8,6 +8,10 @@ def download_dataset(root: str):
     # TODO: df 만드는거 해야함
     os.makedirs(root, exist_ok=True)
 
+    if os.path.exists(os.path.join(root, "Train1_Vibration")):
+        print("이미 데이터셋이 존재합니다. 다운로드를 건너뜁니다.")
+        return
+
     # 다운로드
     url = "https://drive.google.com/uc?id=1ahTu-haFuwS7orW2Dpt8kZG9Di563sNB"
     output = os.path.join(root, "Train.zip")
--- funs/utils.py
+++ funs/utils.py
@@ -1,5 +1,11 @@
 import yaml
+from nptdms import TdmsFile
 
 def load_yaml(yaml_path):
     with open(yaml_path, 'r') as f:
         return yaml.safe_load(f)
+
+def load_tdms_file(file_path):
+    tdms_file = TdmsFile.read(file_path)
+    df = tdms_file.as_dataframe()
+    return df
